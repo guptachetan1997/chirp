948c89817313b917f9f14647efd3838cda46a098
guptachetan1997 <gupta.chetan1997@gmail.com>
2016-06-11 19:33:25 +0530

    default pic try

--- Chirp/urls.py
+++ Chirp/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url,include
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.staticfiles import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -25,5 +26,9 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [
+        url(r'^static/(?P<path>.*)$', views.serve),
+    ]
 
 # urlpatterns += patterns('django.contrib.staticfiles.views',url(r'^static/(?P<path>.*)$', 'serve'),)
