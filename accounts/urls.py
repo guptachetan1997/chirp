6194c36eb9dba7437ae3c99d34ff1f41a42e6146
guptachetan1997 <gupta.chetan1997@gmail.com>
2016-06-11 19:38:32 +0530

    removed image for the while and tried for url

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^auth_view', views.auth_view),
     url(r'^logout', views.log_out),
     url(r'^register', views.register),
-    url(r'^profile/user/(?P<user_username>\w+)$', views.user_profile_display),
+    url(r'^profile/user/(?P<user_username>[\w.]+)$', views.user_profile_display),
     url(r'^profile/edit', views.user_profile_edit),
     url(r'^profile/follow', views.user_follow),
     url(r'^profile/unfollow', views.user_unfollow),
